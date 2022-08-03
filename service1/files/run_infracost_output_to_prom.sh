4abb19166bc5ab16f3f37e3f3bb0df85ba3afc04
Adam Gardner <adam@agardner.net>
2022-08-03 15:16:00 +1000

    Update and rename run_infracost_output_to_prom.sh to run_infracost_output_to_prom.py

--- service1/files/run_infracost_output_to_prom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "hello world"
-#printenv
-# env var from secret
-INFRACOST_OUTPUT=$(curl -X POST -H "x-api-key: $INFRACOST_API_KEY" -F "ci-platform=keptn" \
--F "path=@/keptn/files/plan.json" \
-https://pricing.api.infracost.io/diff)
-echo "Printing Infracost Output..."
-echo $INFRACOST_OUTPUT
