d2e8639a39491249e92715eab2105b2b49464afa
Adam Gardner <adam@agardner.net>
2022-08-03 15:23:11 +1000

    Update run_infracost_output_to_prom.py

--- service1/files/run_infracost_output_to_prom.py
+++ service1/files/run_infracost_output_to_prom.py
@@ -8,18 +8,3 @@ reg = CollectorRegistry()
 g = Gauge(name='dummy_python_metrics', registry=reg, labelnames=["label1", "label2"],labelvalues=["val1","val2"])
 g.set(99)
 push_to_gateway(gateway='prometheus-pushgateway.monitoring.svc.cluster.local:9091',job='batchA', registry=reg)
-
-exit()
-
-
-api_response = requests.post()
-
-#!/bin/sh
-echo "hello world"
-#printenv
-# env var from secret
-INFRACOST_OUTPUT=$(curl -X POST -H "x-api-key: $INFRACOST_API_KEY" -F "ci-platform=keptn" \
--F "path=@/keptn/files/plan.json" \
-https://pricing.api.infracost.io/diff)
-echo "Printing Infracost Output..."
-echo $INFRACOST_OUTPUT
