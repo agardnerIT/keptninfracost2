471c29014ecf36ba95d69e0397730b9fb4156be7
Adam Gardner <adam@agardner.net>
2022-08-03 15:58:46 +1000

    Update run_infracost_output_to_prom.py

--- service1/files/run_infracost_output_to_prom.py
+++ service1/files/run_infracost_output_to_prom.py
@@ -5,7 +5,7 @@ from prometheus_client import CollectorRegistry, Counter, Gauge, push_to_gateway
 print("Hello world...")
 reg = CollectorRegistry()
 
-g = Gauge(name='dummy_python_metrics', registry=reg, labelnames=["label1", "label2"])
+g = Gauge(name='dummy_python_metrics', documentation='docs', registry=reg, labelnames=["label1", "label2"])
 g.labels(label1="foo", label2="bar").set(99)
 
 push_to_gateway(gateway='prometheus-pushgateway.monitoring.svc.cluster.local:9091',job='batchA', registry=reg)
