6759bc2ce02a761ac4f787ef170a181093800d78
Adam Gardner <adam@agardner.net>
2022-08-03 13:01:59 +1000

    Update run_infracost_output_to_prom.sh

--- service1/files/run_infracost_output_to_prom.sh
+++ service1/files/run_infracost_output_to_prom.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-apk add curl
-curl -X GET https://webhook.site/45796edd-3b62-4bc5-8810-eee105553758
+echo "hello world"
+#apk add curl
+#curl -X GET https://webhook.site/45796edd-3b62-4bc5-8810-eee105553758
