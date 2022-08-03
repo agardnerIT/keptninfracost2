1c8c62c14bf6c45111b3db1eb7e3af0fa3bae82d
Adam Gardner <adam@agardner.net>
2022-08-03 13:27:33 +1000

    Update run_infracost_output_to_prom.sh

--- service1/files/run_infracost_output_to_prom.sh
+++ service1/files/run_infracost_output_to_prom.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 echo "hello world"
-printenv
-curl -X GET https://webhook.site/45796edd-3b62-4bc5-8810-eee105553758
+#printenv
+# env var from secret
+INFRACOST_OUTPUT=$(curl -X POST -H "x-api-key: $INFRACOST_API_KEY" -F "ci-platform=keptn" \
+-F "path=@/keptn/files/plan.json" \
+https://pricing.api.infracost.io/diff)
+echo "Printing Infracost Output..."
+echo $INFRACOST_OUTPUT
